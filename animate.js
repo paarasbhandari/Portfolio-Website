b0534ca10a82eab1d14353037dbcb23b150149eb
paarasbhandari <31348197+paarasbhandari@users.noreply.github.com>
2018-06-28 09:44:03 +0530

    Update animate.js

--- animate.js
+++ animate.js
@@ -1,10 +1,7 @@
-<script language="JavaScript" type="text/javascript" src="scripts/jquery.js"></script>
-<script language="JavaScript" type="text/javascript" src="scripts/bootstrap.min.js"></script>
-  <!-- Carousel -->
 <script language="JavaScript" type="text/javascript">
   $(document).ready(function(){
     $('#Carousel').carousel({
-      interval: 3000
+      interval: 2000
     })
   });    
 </script>  
